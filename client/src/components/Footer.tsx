79a039971e3a537a77990059192fe1485c74d8f2
emanuelefavero <emafaux89@gmail.com>
2022-11-25 03:52:34 +0100

    client - styling - footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,22 +1,20 @@
+import styles from './Footer.module.css'
+
 function Footer() {
   return (
-    <div>
-      <p>Emanuele Favero @ {new Date().getFullYear()}</p>
-      <h4>Contact Me:</h4>
+    <div className={styles.footer}>
       <ul>
         <li>
+          <h4>Contact Me:</h4>
           <a href='https://github.com/emanuelefavero'>Github</a>
-        </li>
-        <li>
           <a href='https://www.linkedin.com/in/emanuele-favero/'>Linkedin</a>
         </li>
         <li>
           <a href='https://emanuelefavero.com/'>Website</a>
-        </li>
-        <li>
           <a href='mailto:info@emanuelefavero.com'>Email me</a>
         </li>
       </ul>
+      <p>Emanuele Favero @ {new Date().getFullYear()}</p>
     </div>
   )
 }
