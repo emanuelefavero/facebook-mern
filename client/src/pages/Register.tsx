a16b384172bcbe244ae0fcd9cc85d51b0b663043
emanuelefavero <emafaux89@gmail.com>
2022-11-27 20:38:47 +0100

    client - styling - register page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,3 +1,4 @@
+import styles from './Register.module.css'
 import { useContext, useEffect } from 'react'
 import { useNavigate, Navigate, Link } from 'react-router-dom'
 import UserContext from '../context/UserContext'
@@ -25,40 +26,41 @@ function Register() {
     return <Navigate to='/' replace />
   } else {
     return (
-      <div>
+      <div className={styles.register}>
         <header>Join Facebook</header>
-        <h1>Choose a username: </h1>
+        <h2>Choose a username and password: </h2>
         <p>Enter a unique username to use on our site.</p>
-        <label htmlFor='username'>Username</label>
-        <input
-          name='username'
-          type='text'
-          placeholder='Username'
-          onChange={(e) => setRegisterUsername(e.target.value)}
-          value={registerUsername}
-        />
-        <label htmlFor='username'>Password</label>
-        <input
-          name='password'
-          type='text'
-          placeholder='Password'
-          onChange={(e) => setRegisterPassword(e.target.value)}
-          value={registerPassword}
-        />
+        <div className={styles.inputsContainer}>
+          <div className={styles.inputContainer}>
+            <label htmlFor='username'>Username</label>
+            <input
+              name='username'
+              type='text'
+              onChange={(e) => setRegisterUsername(e.target.value)}
+              value={registerUsername}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor='password'>Password</label>
+            <input
+              name='password'
+              type='text'
+              onChange={(e) => setRegisterPassword(e.target.value)}
+              value={registerPassword}
+            />
+          </div>
+        </div>
         <button
           onClick={() => {
             register()
             navigate('/')
           }}
         >
-          Register
+          Join
         </button>
-        <div>
-          <br />
-          or
-          <br />
+        <div className={styles.linkContainer}>
+          <Link to='/login'>Already have an account?</Link>
         </div>
-        <Link to='/login'>Login</Link>
       </div>
     )
   }
